d75f8a998c93fff9eaaaedbcac90a78b96fb8f52
Thomas Heaney <99109758+tommyheaney@users.noreply.github.com>
2023-12-06 21:52:11 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class PackageExpress
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+        Console.Write("Please enter the package weight: ");
+        double weight = double.Parse(Console.ReadLine());
+
+        if (weight > 50)
+        {
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            return;
+        }
+
+        Console.Write("Please enter the package width: ");
+        double width = double.Parse(Console.ReadLine());
+
+        Console.Write("Please enter the package height: ");
+        double height = double.Parse(Console.ReadLine());
+
+        Console.Write("Please enter the package length: ");
+        double length = double.Parse(Console.ReadLine());
+
+        if (width + height + length > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
+            return;
+        }
+
+        double quote = (height * width * length * weight) / 100.0;
+        Console.WriteLine($"Your estimated total for shipping this package is: ${quote:0.00}");
+        Console.WriteLine("Thank you!");
+    }
+}
